f9fdc3932cd3b08ad725ed6b881827db44e3e834
Kienroro2003 <nguyentrankien281@gmail.com>
2021-10-13 15:09:35 +0700

    6. Tổng quan JavaScript ngay 13 tháng 10 năm 2021

---
+++ Module 1/6. Tổng quan JavaScript/[Thực hành] Hiển thị đầu ra/javascript.js
@@ -0,0 +1,9 @@
+
+
+function showMessage(){
+    // alert("Hello world");
+    // document.getElementById("messageDiv").innerHTML="Hello";
+    // document.write("Hello");
+    console.log("Hello");
+}
+
