1995ce65e2db192ccca7ff5f650901c7f3b71fd3
Kienroro2003 <91589673+Kienroro2003@users.noreply.github.com>
2021-12-02 17:26:19 +0700

    test1

---
+++ Module 2/src/ss1/hello/hello_kitty.java
@@ -0,0 +1,7 @@
+package ss1.hello;
+
+public class hello_kitty {
+    public static void main(String[] args){
+        System.out.print("Hello");
+    }
+}
--- Module 2/src/stackjava/com/helloworld.java
+++
@@ -1,7 +0,0 @@
-package stackjava.com;
-
-public class helloworld {
-    public static void main(String[] args) {
-        System.out.println("hello");
-    }
-}
