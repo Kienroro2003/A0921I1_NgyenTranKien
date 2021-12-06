521dcab86c8f83c01d3334c95872a79b8a4789e7
Kienroro2003 <91589673+Kienroro2003@users.noreply.github.com>
2021-12-06 21:06:29 +0700

    6/12/2021

---
+++ Module2/src/ss3_Array_loop/Thuc_hanh/find_max_element.java
@@ -0,0 +1,37 @@
+package ss3_Array_loop.Thuc_hanh;
+
+import java.util.Scanner;
+
+public class find_max_element {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int size;
+        int[] array;
+        do {
+            System.out.print("Enter a size : ");
+            size = scanner.nextInt();
+            if (size > 20) {
+                System.out.println("Size should not exceed 20 ");
+            }
+        } while (size > 20);
+        array = new int[size];
+        for (int i = 0; i < array.length; i++) {
+            System.out.print("Enter element " + i + " : ");
+            array[i] = scanner.nextInt();
+        }
+        System.out.print("Property list : ");
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + "\t");
+        }
+        int max = 0;
+        int count = 0;
+        for(int i = 0 ;i<array.length;i++){
+            if(max < array[i]){
+                max = array[i];
+                count = i;
+            }
+        }
+        System.out.println();
+        System.out.println("The largest proprety value in the list is "+max+" , at position : "+count);
+    }
+}
